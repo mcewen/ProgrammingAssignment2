c8c5d31163f672486cf8b31603bce6c3a2017091
mcewen <gdmcewen@gmail.com>
2014-04-24 15:06:26 -0600

    commit on 140424

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
-## Write a short comment describing this function
+## makeCacheMatrix() is a function that generates a list of four
+## funcitons:
+## (1) set = set, used by "x$set()" sets the content of the matrix.
+## (2) get = get, used by "x$get()" gets or returns the content of 
+## the matrix.
+## (3) setinverse = setinverse, used by "x$setinverse()" sets the
+## content of the inverted matrix.
+## (4) getinverse = getinverse, used by "x$getinverse()" gets or
+## returns the content of the inverted matrix.
+##
+## "a <- makeCacheMatrix()" will give a list of four functions that
+## can be called with the $: (see above).
+
 makeCacheMatrix <- function(x = numeric(matrix())) {
         m <- NULL
         set <- function(y) {
@@ -15,9 +25,11 @@ makeCacheMatrix <- function(x = numeric(matrix())) {
              getinverse = getinverse)
 }
 
+## cacheSolve() is another function that looks in the element, 
+## passed in as an argument, and will return a matrix that
+## is the inverse of 'x', or it will calculate it and store it via
+## the appropriate function.
 
-## Write a short comment describing this function
-## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
         if(!is.null(m)) {
