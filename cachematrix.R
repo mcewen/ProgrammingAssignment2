694bde34d79a9296752389026e51c4ce68437bcf
mcewen <gdmcewen@gmail.com>
2014-05-23 12:31:26 -0600

    commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,15 @@
-## makeCacheMatrix() is a function that generates a list of four
-## funcitons:
-## (1) set = set, used by "x$set()" sets the content of the matrix.
-## (2) get = get, used by "x$get()" gets or returns the content of 
-## the matrix.
-## (3) setinverse = setinverse, used by "x$setinverse()" sets the
-## content of the inverted matrix.
-## (4) getinverse = getinverse, used by "x$getinverse()" gets or
-## returns the content of the inverted matrix.
-##
-## "a <- makeCacheMatrix()" will give a list of four functions that
-## can be called with the $: (see above).
+# makeCacheMatrix() is a function that generates a list of four
+# funcitons:
+# (1) set = set, used by "x$set()" sets the content of the matrix.
+# (2) get = get, used by "x$get()" gets or returns the content of 
+# the matrix.
+# (3) setinverse = setinverse, used by "x$setinverse()" sets the
+# content of the inverted matrix.
+# (4) getinverse = getinverse, used by "x$getinverse()" gets or
+# returns the content of the inverted matrix.
+#
+# "a <- makeCacheMatrix()" will give a list of four functions that
+# can be called with the $: (see above).
 
 makeCacheMatrix <- function(x = numeric(matrix())) {
         m <- NULL
@@ -25,10 +25,10 @@ makeCacheMatrix <- function(x = numeric(matrix())) {
              getinverse = getinverse)
 }
 
-## cacheSolve() is another function that looks in the element, 
-## passed in as an argument, and will return a matrix that
-## is the inverse of 'x', or it will calculate it and store it via
-## the appropriate function.
+# cacheSolve() is another function that looks in the element, 
+# passed in as an argument, and will return a matrix that
+# is the inverse of 'x', or it will calculate it and store it via
+# the appropriate function.
 
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
